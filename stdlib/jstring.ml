c05e2b04cf2ec1067dbf03b1538fbc03710896ae
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2010-03-11 15:38:04 +0100

    Missing file.

---
+++ stdlib/jstring.ml
@@ -0,0 +1,42 @@
+(*****************************************************************************)
+(*  ChaML, a type-checker that uses constraints and a kernel language        *)
+(*  Copyright (C) 2010 Jonathan Protzenko                                    *)
+(*                                                                           *)
+(*  This program is free software: you can redistribute it and/or modify     *)
+(*  it under the terms of the GNU General Public License as published by     *)
+(*  the Free Software Foundation, either version 3 of the License, or        *)
+(*  (at your option) any later version.                                      *)
+(*                                                                           *)
+(*  This program is distributed in the hope that it will be useful,          *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *)
+(*  GNU General Public License for more details.                             *)
+(*                                                                           *)
+(*  You should have received a copy of the GNU General Public License        *)
+(*  along with this program.  If not, see <http://www.gnu.org/licenses/>.    *)
+(*                                                                           *)
+(*****************************************************************************)
+
+module Buf: sig
+  type t
+  val create: unit -> t
+  val add: t -> string -> unit
+  val add_list: t -> string list -> unit
+  val flush: t -> string
+end = struct
+  type t = string list ref
+
+  let create () =
+    ref []
+
+  let add buf s =
+    buf := s :: !buf
+
+  let add_list buf l =
+    buf := Jlist.append_rev_front l !buf
+
+  let flush buf =
+    let l = List.rev !buf in
+    buf := [];
+    String.concat "" l
+end
