1727a5ffd75fd2b100768bbfb888fd1e3d354f1e
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2010-03-29 16:12:32 +0200

    Revert "Revert "Remember positions for identifiers.""
    This reverts commit b60584989d9f33a7fafc2789635ad4154110ec19.

--- stdlib/jmap.ml
+++ stdlib/jmap.ml
@@ -24,6 +24,7 @@ module Make: functor (M: Map.S) -> sig
   val inter: 'a M.t -> 'a M.t -> 'a M.t
   val minus: 'a M.t -> 'a M.t -> 'a M.t
   val xor: 'a M.t -> 'a M.t -> 'a M.t
+  val to_list: 'a M.t -> (M.key * 'a) list
   
 end = functor (M: Map.S) -> struct
 
@@ -36,14 +37,17 @@ end = functor (M: Map.S) -> struct
 
   (* m1's values are kept *)
   let inter m1 m2 =
-    M.fold (fun k v m1 -> if M.mem k m2 then m1 else M.remove k m1) m1 m1
+    M.fold (fun k _ m1 -> if M.mem k m2 then m1 else M.remove k m1) m1 m1
 
   (* m1 minus all its elements that also belong to m2 *)
   let minus m1 m2 =
-    M.fold (fun k v m1 -> if M.mem k m2 then M.remove k m1 else m1) m1 m1
+    M.fold (fun k _ m1 -> if M.mem k m2 then M.remove k m1 else m1) m1 m1
 
   let xor m1 m2 =
     (* minus (union m1 m2) (inter m1 m2) *)
     union (minus m2 m1) (minus m1 m2)
+
+  let to_list m1 =
+    M.fold (fun k v acc -> (k,v) :: acc) m1 []
 
 end
